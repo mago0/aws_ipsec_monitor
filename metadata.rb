f9a47637c4436e4a5fc901e133dbb1f869b23fbe
Matt Williams <matt@tilt.com>
2015-02-18 10:32:55 -0500

    Updating license in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'aws_ipsec_monitor'
 maintainer       'Matt Williams'
 maintainer_email 'matt@tilt.com'
-license          'Beerware'
+license          'GNU GPL'
 description      'Installs/Configures aws_ipsec_monitor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
