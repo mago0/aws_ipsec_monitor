9f1657f15792ad6234a15ba8c25710d841a4e03e
Matt Williams <matt@tilt.com>
2015-03-09 17:27:33 -0400

    Allowing for administrative failover

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'matt@tilt.com'
 license          'GNU GPL'
 description      'Installs/Configures aws_ipsec_monitor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.2.0'
